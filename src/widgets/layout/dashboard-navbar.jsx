b84a58969394bef2c19de4f228721e9e91faecd8
ansarullahsadat <ansarullahsadat@gmail.com>
2024-05-18 21:03:29 +0430

    sidenav text and icon update

--- src/widgets/layout/dashboard-navbar.jsx
+++ src/widgets/layout/dashboard-navbar.jsx
@@ -7,6 +7,7 @@ import {
   Input,
   Menu,
   Avatar,
+  Card
 } from "@material-tailwind/react";
 import {
   UserCircleIcon,
@@ -49,7 +50,7 @@ export function DashboardNavbar() {
             <Link to={`/${layout}`}>
               <Typography
                 variant="small"
-                color="blue-gray"
+                // color="blue-gray"
                 className="font-normal opacity-50 transition-all hover:text-blue-500 hover:opacity-100"
               >
                 {layout}
@@ -57,13 +58,15 @@ export function DashboardNavbar() {
             </Link>
             <Typography
               variant="small"
-              color="blue-gray"
+              // color="blue-gray"
               className="font-normal"
             >
               {page}
             </Typography>
           </Breadcrumb>
-          <Typography variant="h6" color="blue-gray">
+          <Typography variant="h6"
+          //  color="blue-gray"
+           >
             {page}
           </Typography>
         </div>
@@ -73,7 +76,7 @@ export function DashboardNavbar() {
           </div>
           <IconButton
             variant="text"
-            color="blue-gray"
+            // color="blue-gray"
             className="grid xl:hidden"
             onClick={() => setOpenSidenav(dispatch, !openSidenav)}
           >
@@ -82,7 +85,7 @@ export function DashboardNavbar() {
           <Link to="/auth/sign-in">
             <Button
               variant="text"
-              color="blue-gray"
+              // color="blue-gray"
               className="hidden items-center gap-1 px-4 xl:flex normal-case"
             >
               <UserCircleIcon className="h-5 w-5 text-blue-gray-500" />
@@ -90,7 +93,7 @@ export function DashboardNavbar() {
             </Button>
             <IconButton
               variant="text"
-              color="blue-gray"
+              // color="blue-gray"
               className="grid xl:hidden"
             >
               <UserCircleIcon className="h-5 w-5 text-blue-gray-500" />
@@ -113,14 +116,14 @@ export function DashboardNavbar() {
                 <div>
                   <Typography
                     variant="small"
-                    color="blue-gray"
+                    // color="blue-gray"
                     className="mb-1 font-normal"
                   >
                     <strong>New message</strong> from Laur
                   </Typography>
                   <Typography
                     variant="small"
-                    color="blue-gray"
+                    // color="blue-gray"
                     className="flex items-center gap-1 text-xs font-normal opacity-60"
                   >
                     <ClockIcon className="h-3.5 w-3.5" /> 13 minutes ago
@@ -137,14 +140,14 @@ export function DashboardNavbar() {
                 <div>
                   <Typography
                     variant="small"
-                    color="blue-gray"
+                    // color="blue-gray"
                     className="mb-1 font-normal"
                   >
                     <strong>New album</strong> by Travis Scott
                   </Typography>
                   <Typography
                     variant="small"
-                    color="blue-gray"
+                    // color="blue-gray"
                     className="flex items-center gap-1 text-xs font-normal opacity-60"
                   >
                     <ClockIcon className="h-3.5 w-3.5" /> 1 day ago
@@ -158,14 +161,14 @@ export function DashboardNavbar() {
                 <div>
                   <Typography
                     variant="small"
-                    color="blue-gray"
+                    // color="blue-gray"
                     className="mb-1 font-normal"
                   >
                     Payment successfully completed
                   </Typography>
                   <Typography
                     variant="small"
-                    color="blue-gray"
+                    // color="blue-gray"
                     className="flex items-center gap-1 text-xs font-normal opacity-60"
                   >
                     <ClockIcon className="h-3.5 w-3.5" /> 2 days ago
@@ -176,7 +179,7 @@ export function DashboardNavbar() {
           </Menu>
           <IconButton
             variant="text"
-            color="blue-gray"
+            // color="blue-gray"
             onClick={() => setOpenConfigurator(dispatch, true)}
           >
             <Cog6ToothIcon className="h-5 w-5 text-blue-gray-500" />
--- src/widgets/layout/sidenav.jsx
+++ src/widgets/layout/sidenav.jsx
@@ -8,57 +8,41 @@ import {
   Button,
   Avatar,
 } from "@material-tailwind/react";
-import {
-  Archive,
-  EmptyPage,
-  Folder,
-  LogOut,
-  Mail,
-  MoreHorizCircle,
-  NavArrowRight,
-  Pin,
-  SelectFace3d,
-  SendDiagonal,
-  Trash,
-  UserXmark,
-} from "iconoir-react";
+
+import { HomeAlt , ProfileCircle ,PenTablet ,ChatBubbleWarning , LogIn , DataTransferDown } from "iconoir-react";
+
 import { useMaterialTailwindController, setOpenSidenav } from "@/context";
-import { Link, NavLink } from "react-router-dom";
+import { Link,  } from "react-router-dom";
+
 
 const Links = [
   {
-    icon: Mail,
-    title: "Inbox",
-    href: "#",
-    badge: 14,
+    icon: HomeAlt,
+    title: "Dashboard",
+    href: "../../pages/dashboard/home",
+   
   },
   {
-    icon: SendDiagonal,
-    title: "Sent",
-    href: "#",
+    icon: ProfileCircle,
+    title: "Profile",
+    href: "../../pages/dashboard/profile",
   },
   {
-    icon: EmptyPage,
-    title: "Drafts",
-    href: "#",
+    icon: PenTablet,
+    title: "Tables",
+    href: "../../pages/dashboard/tables",
   },
   {
-    icon: Pin,
-    title: "Pins",
-    href: "#",
+    icon: ChatBubbleWarning,
+    title: "Notifications",
+    href: "../../pages/dashboard/notifications",
   },
-  {
-    icon: Archive,
-    title: "Archive",
-    href: "#",
-  },
-  {
-    icon: Trash,
-    title: "Trash",
-    href: "#",
-  },
+  
 ];
  
+// import file from "../../pages/dashboard/profile"
+
+
 export function Sidenav({ brandName, routes }) {
   const [isOpen, setIsOpen] = React.useState(false);
   
@@ -76,7 +60,7 @@ export function Sidenav({ brandName, routes }) {
       } shadow-none fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 transition-transform duration-300 xl:translate-x-0`}>
        <Card.Header className="mx-3 mb-1 mt-3 flex h-max items-center gap-3">
         <Avatar size="sm" src="https://mtr-v3-site.vercel.app/logo.png" alt="brand" />
-        <Typography className="font-bold">
+        <Typography className="font-bold ">
           Material Tailwind
         </Typography>
       </Card.Header> 
@@ -127,62 +111,34 @@ export function Sidenav({ brandName, routes }) {
             ))}</>
           ))}   */}
           {/* problem is here */}
-           {Links.map(({ icon: Icon, title, href, badge }) => (
-            <List.Item key={title} href={href}>
+           {Links.map(({ icon: Icon, title, href }) => (
+            <List.Item key={title} href={href} className="cursor-pointer py-4 font-semibold flex items-center hover:bg-slate-200 px-3 rounded-md ">
               <List.ItemStart>
                 <Icon className="h-[18px] w-[18px]" />
               </List.ItemStart>
-              {title}
-              {badge && (
-                <List.ItemEnd>
-                  <Chip size="sm" variant="ghost">
-                    <Chip.Label>{badge}</Chip.Label>
-                  </Chip>
-                </List.ItemEnd>
-              )}
+           {title}
             </List.Item>
           ))}
           <hr className="-mx-3 my-3 border-secondary" />
-          <List.Item onClick={() => setIsOpen((cur) => !cur)}>
-            <List.ItemStart>
-              <MoreHorizCircle className="h-[18px] w-[18px]" />
-            </List.ItemStart>
-            More
-            <List.ItemEnd>
-              <NavArrowRight
-                className={`h-4 w-4 ${isOpen ? "rotate-90" : ""}`}
-              />
-            </List.ItemEnd>
-          </List.Item>
+          <List.Item onClick={() => setIsOpen((cur) => !cur)} className="cursor-pointer font-semibold hover:bg-slate-200 p-4">
+                         Auth Pages
+             </List.Item>
           <Collapse open={isOpen}>
             <List>
-              <List.Item>
+              <List.Item className="cursor-pointer font-semibold hover:bg-slate-200 p-4">
                 <List.ItemStart>
-                  <Folder className="h-[18px] w-[18px]" />
+                  <LogIn className="h-[18px] w-[18px]" />
                 </List.ItemStart>
-                Spam
+                <Link>Sign in</Link>
               </List.Item>
-              <List.Item>
+              <List.Item className="cursor-pointer font-semibold hover:bg-slate-200 p-4">
                 <List.ItemStart>
-                  <UserXmark className="h-[18px] w-[18px]" />
+                  <DataTransferDown className="h-[18px] w-[18px]" />
                 </List.ItemStart>
-                Blocked
-              </List.Item>
-              <List.Item>
-                <List.ItemStart>
-                  <Folder className="h-[18px] w-[18px]" />
-                </List.ItemStart>
-                Important
+               <Link>Sign Up</Link>
               </List.Item>
             </List>
           </Collapse>
-          <hr className="-mx-3 my-3 border-secondary" />
-          <List.Item className="text-error hover:bg-error/10 hover:text-error focus:bg-error/10 focus:text-error">
-            <List.ItemStart>
-              <LogOut className="h-[18px] w-[18px]" />
-            </List.ItemStart>
-            Logout
-          </List.Item> 
          </List>
         
         </Card.Body>
