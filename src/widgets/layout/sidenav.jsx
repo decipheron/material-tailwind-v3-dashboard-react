1c0010a1658448dab1e50741f4e122963d1c146d
ansarullahsadat <ansarullahsadat@gmail.com>
2024-05-21 00:21:31 +0700

    sidenav update

--- src/widgets/layout/sidenav.jsx
+++ src/widgets/layout/sidenav.jsx
@@ -39,6 +39,10 @@ const Links = [
   },
 
   {
+    
+  },
+
+  {
     icon : LogIn,
     title: "Sign in",
     href : "/auth/sign-in"
@@ -54,10 +58,11 @@ const Links = [
 
 
 
+
+
 export function Sidenav({ brandName, routes }) {
   const [isOpen, setIsOpen] = React.useState(false);
-  
-   const [controller, dispatch] = useMaterialTailwindController();
+  const [controller, dispatch] = useMaterialTailwindController();
   const { sidenavColor, sidenavType, openSidenav } = controller;
   const sidenavTypes = {
     dark: "bg-gradient-to-br from-gray-800 to-gray-900",
@@ -66,22 +71,22 @@ export function Sidenav({ brandName, routes }) {
   };
 
   return (
-    <Card className={`${sidenavTypes[sidenavType]} ${openSidenav ? "translate-x-0" : "-translate-x-80"} shadow-none fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 transition-transform duration-300 xl:translate-x-0`}>
+    <Card className={`${sidenavTypes[sidenavType]} ${openSidenav ? "translate-x-0" : "-translate-x-80"} shadow-none fixed inset-0  py-4 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 transition-transform duration-300 xl:translate-x-0`}>
        <Card.Header className="mx-3 mb-1 mt-3 flex h-max items-center gap-3">
-        <Avatar size="sm" src="https://mtr-v3-site.vercel.app/logo.png" alt="brand" />
+        <Avatar size="sm" src="https://mtr-v3-site.vercel.app/logo.png" alt="brand"/>
         <Typography className="font-bold ">
           Material Tailwind
         </Typography>
       </Card.Header> 
-      <Card.Body className="p-3">
+      <Card.Body className="p-3 mt-4">
          <List>
            {Links.map(({ icon: Icon, title, href }) => (
-            <Link to={href}>
-            <List.Item key={title} className="cursor-pointer py-4 font-semibold flex items-center hover:bg-slate-200 px-3 rounded-md ">
+            <Link to={href} >
+            <List.Item key={title} className="cursor-pointer py-4 font-semibold flex items-center hover:bg-slate-200 px-3 rounded-md">
               <List.ItemStart>
-                <Icon className="h-[18px] w-[18px]" />
+               {Icon &&  <Icon className="h-[18px] w-[18px]"/>}
               </List.ItemStart>
-           {title}
+           {title ? title : <span className="text-sm uppercase -mx-2">Auth Pages</span>}
             </List.Item>
             </Link>
           ))}
